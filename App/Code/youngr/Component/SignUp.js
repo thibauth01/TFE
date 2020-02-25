ed778983eea85a00f4e2300d5d6c18e7afc3d24b
Thibaut Hermant <hermantthibaut@gmail.com>
2020-02-25 14:11:05 +0100

    SignUp view

--- App/Code/youngr/Component/SignUp.js
+++ App/Code/youngr/Component/SignUp.js
@@ -22,6 +22,17 @@ class SignUp extends React.Component {
           <View style={styles.logo_view}>
             <Image style={styles.logo_img} source={require('../Images/logo_youngr.png')}/>
           </View>
+          <View tyle={styles.header_text_view}>
+            <View style={styles.header_text_view_1}>
+              <Text style={styles.header_text_1}>Looking for a service or a job ?</Text>
+            </View>
+            <View style={styles.header_text_view_2}>
+              <Text style={styles.header_text_2}>________</Text>
+            </View>
+            <View style={styles.header_text_view_3}>
+              <Text style={styles.header_text_3}>To facilitate liaison between {"\n"}young workers and requesters</Text>
+            </View>
+          </View>
         </View>
 
         <View style={styles.body_container}>
@@ -37,7 +48,8 @@ const styles = StyleSheet.create({
    
   main_container: {
     flex: 1,
-    backgroundColor:"#334856"
+    backgroundColor:"#334856",
+    fontFamily:"Roboto"
   },
 
   /*Header*/
@@ -55,8 +67,46 @@ const styles = StyleSheet.create({
     height:100,
     width:250
     
+  },
+  header_text_view:{
 
   },
+  header_text_view_1:{
+    justifyContent:"center",
+    alignItems:"flex-start",
+    marginLeft:10
+    
+
+  },
+  header_text_1:{
+    color:"white",
+    fontSize:23,
+    
+    
+  },
+  header_text_view_2:{
+    justifyContent:"center",
+    alignItems:"flex-start",
+    marginLeft:10,
+    marginTop:15
+  },
+  header_text_2:{
+    color:"white",
+    fontSize:10,
+
+  },
+  header_text_view_3:{
+    justifyContent:"center",
+    alignItems:"flex-start",
+    marginLeft:10,
+    marginTop:15
+  },
+  header_text_3:{
+    color:"white",
+    fontSize:15,
+
+  },
+  
 
   /*Body*/
   body_container:{
