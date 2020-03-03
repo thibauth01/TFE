ae115cbf584c7462a81cb05b3e0b7094ad663fa3
Thibaut Hermant <hermantthibaut@gmail.com>
2020-03-03 18:38:35 +0100

    connexion

--- App/Code/youngr/Component/Connexion.js
+++ App/Code/youngr/Component/Connexion.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Text , Platform, Button, TouchableOpacity, Alert, Image, Dimensions  } from 'react-native'
+import { StyleSheet, View, TextInput, Text , Platform, TouchableOpacity, Alert, Image, Dimensions  } from 'react-native'
+import { Button } from 'react-native-elements'
 import { LogIn } from './LogIn'
 import {createAppContainer} from 'react-navigation'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
@@ -18,17 +19,31 @@ class Connexion extends React.Component {
     return (
       <View style={styles.main_container}>
         
-        <View style={styles.body_container}>
+        <View style={styles.view_image}>
           <Image style={styles.image} source={require('../Images/logo_youngr.png')} />
         </View>
-        <Button
-            title="LogIn"
-            onPress={() => navigate('LogIn')}
-          />
-        <Button
-          title="Sign Up"
-          onPress={() => navigate('SignUp')}
-        />
+    
+        <View style={styles.view_buttons}>
+          <View styles={styles.view_button_logIn}>
+            <Button
+              type="clear"
+              buttonStyle={styles.button_style}
+              titleStyle={styles.title_button_style_logIn}
+              title="Log In"
+              onPress={() => navigate('LogIn')}
+              />
+          </View>
+          <View styles={styles.view_button_signUp}>
+            <Button
+              type="clear"
+              titleStyle={styles.title_button_style_signUp}
+              buttonStyle={styles.button_style}
+              title="Sign Up"
+              onPress={() => navigate('SignUp')}
+            />
+          </View>
+        </View>
+      
 
       </View>
     )
@@ -52,9 +67,45 @@ const styles = StyleSheet.create({
     
 
   },
+  view_image:{
+    flex:3,
+    justifyContent:"center"
+    
+  },
   image:{
     width:Dimensions.get("window").width,
     height:100,
+    
+  },
+ 
+  view_buttons:{
+    width:Dimensions.get("window").width,
+    flex:1,
+    flexDirection:'row',
+    justifyContent:"space-around",
+    alignItems:"center",
+    
+  },
+  view_button_logIn:{
+    
+
+  },
+  view_button_signUp:{
+    
+
+  },
+
+  button_style:{
+    width:150,
+    height:70,
+  },
+  title_button_style_logIn:{
+    color:'white',
+    fontSize:20
+  },
+  title_button_style_signUp:{
+    color:'#D97D54',
+    fontSize:20
   }
 
 })
