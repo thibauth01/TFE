ad956cb7dc3da1a160ca09fa7a9727bfb6dbce85
Thibaut Hermant <hermantthibaut@gmail.com>
2020-04-10 00:50:58 +0200

    Details Work

--- App/Code/youngr/Component/Dashboard.js
+++ App/Code/youngr/Component/Dashboard.js
@@ -13,6 +13,25 @@ import {
 } from "react-native-chart-kit";
 
 
+const labelsGraph=["J","F","M","A","M","J","J","A","S","O","N","D"];
+const datasGraph=[
+  {
+    data: [
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10,
+      Math.random() * 10
+    ]
+  }
+];
 
 const dataJobsFree = [
   {
@@ -85,7 +104,39 @@ class Dashboard extends React.Component {
               
           </Block>
           <Block flex={3}>
-              
+            <Text h4 muted style={styles.subtitle}>Revenus</Text>
+            <LineChart
+              data={{
+                labels:labelsGraph,
+                datasets: datasGraph
+              }}
+              width={(Dimensions.get("window").width)-10} // from react-native
+              height={220}
+              yAxisLabel="€"
+              yAxisSuffix=""
+              yAxisInterval={2} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: theme.COLORS.SECONDARY,
+                backgroundGradientFrom: theme.COLORS.SECONDARY,
+                backgroundGradientTo: theme.COLORS.SECONDARY,
+                decimalPlaces: 0, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style:{
+                  
+                },
+                propsForDots: {
+                  r: "2",
+                  strokeWidth: "2",
+                  stroke: "#fff"
+                }
+              }}
+              withShadow
+              bezier
+              style={{
+                borderRadius: 10
+              }}
+            />
           </Block>
 
         </Block>
--- App/Code/youngr/Component/DetailsWork.js
+++ App/Code/youngr/Component/DetailsWork.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet,Image,View,Platform,SafeAreaView, ImageBackground,Linking } from 'react-native'
-import {Button,Text, Block, Input} from 'galio-framework'
+import { StyleSheet,Image,View,Platform,SafeAreaView, ImageBackground,Linking, Dimensions, ScrollView, TouchableOpacity } from 'react-native'
+import {Button,Text, Block, Input, Icon} from 'galio-framework'
 import { theme } from '../Constants';
 
 
@@ -13,29 +13,69 @@ class Message extends React.Component {
 
   render() {
     const {state} = this.props.navigation;
+    
     return (
-        <Block middle style={styles.main_container}>
-          <Block flex={1} style={styles.headerBlock}>
+        <Block style={styles.main_container}>
+          <Block flex={1.2} style={styles.headerBlock}>
             <Block flex={1} style={styles.profileBlock}>
-              <Image style={styles.profile} source={require(`../Images/testProfile.jpeg`)}></Image>
+              <Image style={styles.profile} source={require(`../Images/avatar.jpg`)}></Image>
             </Block>
-            <Block  flex={2} style={styles.infoprofileBlock}>
-              <Block row>
-                <Text h3 color={theme.COLORS.MUTED}>Jason</Text> 
-                <Text h3 color={theme.COLORS.MUTED}> Statam</Text>
+            <Block flex={2} style={styles.infoprofileBlock}>
+              <Block row  space="around" width={200}>
+                <Text h3 muted>Jason</Text> 
+                <Text h3 muted> Statam</Text>
               </Block> 
-              <Block row>
-                <Text> 22 ans</Text>
-                <Text> Louvain-la-Neuve</Text>
+              <Block row space="around" width={200}>
+                <Text muted > 22 ans</Text>
+                <Text muted> Louvain-la-Neuve</Text>
               </Block>
-              <Block row>
-                <Button> Message</Button>
-                <Button> Phone</Button>
+              <Block row style={styles.buttonsProfile}>
+                <Button style={styles.buttonProfileMessage}  iconColor={theme.COLORS.SECONDARY}	onlyIcon iconSize={20} icon="message-circle" iconFamily="feather" flex={2}></Button>
+                <Button style={styles.buttonProfilePhone}  iconColor={theme.COLORS.DEFAULT}	onlyIcon iconSize={20} icon="phone" iconFamily="feather" flex={2}></Button>
               </Block>      
             </Block>
           </Block>
-          <Block flex={1} style={styles.titleBlock}>
-
+          <Block flex={0.9} space="evenly" center fluid style={styles.titlesBlock}>
+              <Text style={styles.textTitle} h3>Nettoie sale fdp</Text>
+              <Text style={styles.textTitle} h5 color={theme.COLORS.SECONDARY}>Bricolage</Text>
+          </Block>
+          
+          <Block flex={3} style={styles.contentBlock}>
+            <ScrollView >
+              <Block row style={styles.itemBlock}>
+                <Icon  size={25} name="calendar" family="feather" color={theme.COLORS.MUTED}/>
+                <Text  style={styles.textContent}>20 Mars 2020</Text>
+                <Block middle style={styles.progressionBlock}>
+                  <Text color={theme.COLORS.GITHUB} style={styles.progressionText}>Terminé </Text>
+                </Block>
+              </Block>
+              <Block row style={styles.itemBlock}>
+                <Icon size={25} name="clock" family="feather" color={theme.COLORS.MUTED}/>
+                <Text style={styles.textContent}>20h30 - 22h30</Text>
+              </Block>
+              <Block row style={styles.itemBlock}>
+                <Icon size={25} name="dollar-sign" family="feather" color={theme.COLORS.MUTED}/>
+                <Text style={styles.textContent}>33€</Text>
+              </Block>
+              <Block row style={styles.itemBlock}>
+                <Icon size={25} name="map-pin" family="feather" color={theme.COLORS.MUTED}/>
+                <Text style={styles.textContent}>3 Rue des cochons 6440 Louvain la Neuve </Text>
+              </Block>
+              <Block row style={styles.itemBlock}>
+                <Icon size={25} name="tag" family="feather" color={theme.COLORS.MUTED}/>
+                <Text style={styles.textContent}>3 Rue des cochons 6440 Louvain-la Neuve fuerifurieeeeeeee eeeeeeeeee eeeeeeeeeeeeegulbvreilvsjbfttd gshtrd gsht rsht </Text>
+              </Block>
+            </ScrollView>
+          </Block>
+          
+          <Block row space="evenly" flex={0.5}>
+            <TouchableOpacity >
+              <Icon name="x" family="feather" size={25} color={theme.COLORS.ERROR}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="check" family="feather" size={25} color={theme.COLORS.SUCCESS}/>
+            </TouchableOpacity>
+            
           </Block>
         </Block>
     )
@@ -43,17 +83,32 @@ class Message extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
+  test:{
+    backgroundColor:'red',
+    
+  },
   main_container: {
     flex:1,
     backgroundColor:theme.COLORS.BACKGROUND
     
   },
   headerBlock:{
-    flexDirection:"row"
+    flexDirection:"row",
+    backgroundColor:theme.COLORS.DEFAULT,
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 1,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 1.41,
+
+    elevation: 5,
   },
   profile:{
-    borderRadius:200
+    borderRadius:200,
+    height:100,
+    width:100
   },
   profileBlock:{
     marginTop:15,
@@ -61,11 +116,70 @@ const styles = StyleSheet.create({
 
   },
   infoprofileBlock:{
-
+    paddingTop:15
   },
-  titleBlock:{
+  
+  buttonsProfile:{
+    width:200,
+    paddingTop:10,
+    justifyContent:"space-around",
+  },
+  buttonProfileMessage:{
+    width:80,
+    height:35,
+    backgroundColor: theme.COLORS.DEFAULT,
+    borderBottomColor:theme.COLORS.MUTED,
+    
+  },
+  buttonProfilePhone:{
+    width:80,
+    height:35,
+    backgroundColor: theme.COLORS.SECONDARY
+  },
 
+  titlesBlock:{
+    width:Dimensions.get("window").width,
+    marginTop:10
+    
+  },
+  textTitle:{
+    textAlign:"center"
+  },
+
+  contentBlock:{
+    width:Dimensions.get("window").width,
+    paddingRight:30,
+  },
+  itemBlock:{
+    marginTop:15,
+    marginLeft:25
+  },
+  textContent:{
+    fontSize:18,
+    marginLeft:15
+  },
+  progressionBlock:{
+    backgroundColor:theme.COLORS.DEFAULT,
+    width:80,
+    height:30,
+    marginLeft:60,
+    borderRadius:5,
+  },
+  progressionText:{
+    color:theme.COLORS.SUCCESS
+  },
+  buttonDelete:{
+    backgroundColor:theme.COLORS.ERROR,
+    width:80,
+    height:35,
+  },
+  buttonAccept:{
+    backgroundColor:theme.COLORS.SUCCESS,
+    width:80,
+    height:35,
   }
+
+
 
 })
 
